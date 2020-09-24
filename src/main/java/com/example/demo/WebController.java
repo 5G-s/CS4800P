dc3856cb827af9f8ee4c98d0f2989db18a52a1ec
Cruz <cruzmax21@gmail.com>
2020-09-24 02:00:24 -0700

    localhost connected

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -1,9 +1,13 @@
 package com.example.demo;
+
+import com.github.ankurpathak.username.bean.constraints.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
@@ -11,12 +15,24 @@ public class WebController {
 
 
     public static void main(String[] args) {
+
         SpringApplication.run(WebController.class, args);
     }
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+
+
+//    StartWithAlphabet to check if username start with alphabet
+    private String verifyName() {
+        @StartWithAlphabet
+
+        String users = null;
+        return users;
+
     }
 
 }
